860eb2c81d5306ef64a04860b1f4d01f42bcba70
Shubham Raj <89620610+D3vil-Sr@users.noreply.github.com>
2022-07-15 10:32:00 +0530

    Update Ractangle_pattern.cpp

--- Ractangle_pattern.cpp
+++ Ractangle_pattern.cpp
@@ -4,8 +4,9 @@ using namespace std;
 int main()
 {
   int n;
+  cout << "HOW MANY ROWS:-" << endl;
   cin >> n;
-  for (int i = 0; i <= n; i++)
+  for (int i = 1; i <= n; i++)
   {
     for (int j = 1; j < n; j++)
     {
